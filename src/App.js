33220ab84567ce5b9fccbb9f3f2aceab14f8ce72
ansdo0606 <ansdo0606@gmail.com>
2023-07-23 22:16:28 +0900

    구조분해할당/useEffect 등
    useEffect의 장점 , 디자인 등

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import './App.css';
-import {useState} from 'react';
-function Counter(props){
-  let countState = useState(props.initValue);
-  let count = countState[0];
-  let setCount = countState[1];
+import {useState, useEffect} from 'react';
+function Counter({title, initValue}){
+  const [count, setCount] = useState(initValue);
+  const [step, setStep] = useState(1);
+  const [history, setHistory] = useState([5,5]);
   function up(){
-    setCount(count+1);
+    const newCount = count + step;
+    setCount(newCount);
+    const newHistory = [...history];
+    newHistory.push(newCount);
+    setHistory(newHistory);
   }
-  return <div>
-    <h1>{props.title}</h1>
-    <button onClick={up}>+</button> {count}
+  const stepHandler = (evt) => {
+    setStep(Number(evt.target.value));
+  };
+  const style = { border: '10px solid black', padding:10, backgroundColor:'tomato' };
+  return <div style={style}>
+    <h1>{title}</h1>
+    <button onClick={up}>+</button> 
+    <input type="number" value={step} onChange={stepHandler}/>
+    {count}
+    <ol>
+      {history.map((e,index)=><li key={index}>{e}</li>)}
+    </ol>
+  </div>
+}
+function CounterUseEffect(){
+  const [count, setCount] = useState(0);
+  useEffect(()=>{
+    console.log('useEffect');
+    const id = setInterval(()=>{
+      console.log('interval');
+      setCount(oldCount=>oldCount+1);
+    }, 1000)
+    return ()=>{
+      console.log('clean');
+      clearInterval(id);
+    }
+  },[]);//빈 배열을 넣으면 딱 한번만 실행된다. 배열안에 count를 넣으면 count가 바뀔때만 실행된다.
+  const style = { border: '10px solid black', padding:10 };
+  return <div style={style}>
+    <h1>useEffect Counter</h1> {count}
   </div>
 }
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
+      <CounterUseEffect></CounterUseEffect>
     </div>
   );
 }
